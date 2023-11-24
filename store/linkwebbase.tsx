3a1af302d2921fef12260b6a4d0567b3507a4186
Lê Bảo Long <longle12042006a@gmail.com>
2023-11-25 00:49:06 +0700

    update url

--- store/linkwebbase.tsx
+++ store/linkwebbase.tsx
@@ -1,3 +1,9 @@
-const urlbase = "http://localhost:3000/";
+let hostname;
+
+if (typeof window !== 'undefined') {
+    hostname = window.location.hostname;
+}
+
+const urlbase = hostname || 'https://doantruongctdn-website.vercel.app/'; // Provide a default value if hostname is undefined
 
 export default urlbase;
