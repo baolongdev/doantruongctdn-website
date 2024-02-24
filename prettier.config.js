bb5805cee7edde7a72416496b61a668a8595d6b6
Lê Bảo Long <longle12042006a@gmail.com>
2024-02-24 17:38:31 +0700

    abc

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    arrowParens: 'avoid',
-    singleQuote: true,
-    bracketSpacing: true,
-    ebdOfLine: 'lf',
-    semi: false,
-    tabWidth: 4,
-    trailingComma: 'none',
-}
