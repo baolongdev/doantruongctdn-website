fa8815ff3bfa99e69b63ae2f82a6d373a2f610b3
Lê Bảo Long <longle12042006a@gmail.com>
2024-02-24 19:19:45 +0700

    fix lần nữa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,8 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mixitup/3.3.1/mixitup.min.js"></script>
         <script
           dangerouslySetInnerHTML={{
@@ -17,6 +19,7 @@ export default function Document() {
           `,
           }}
         ></script>
+
       </Head>
       <body>
         <Main />
